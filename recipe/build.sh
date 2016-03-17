88d8f50802dcad7843edbd8e11c8e10a84239714
bekozi <benkoziol@gmail.com>
2016-03-17 15:13:28 -0600

    Added location prefixes for lib, mod, doc, etc.

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,11 @@ fi
 
 export ESMF_DIR=$(pwd)
 export ESMF_INSTALL_PREFIX=${PREFIX}
+export ESMF_INSTALL_BINDIR=${PREFIX}/bin
+export ESMF_INSTALL_DOCDIR=${PREFIX}/doc
+export ESMF_INSTALL_HEADERDIR=${PREFIX}/include
+export ESMF_INSTALL_LIBDIR=${PREFIX}/lib
+export ESMF_INSTALL_MODDIR=${PREFIX}/mod
 export ESMF_NETCDF="split"
 export ESMF_NETCDF_INCLUDE=${PREFIX}/include
 export ESMF_NETCDF_LIBPATH=${PREFIX}/lib
@@ -15,14 +20,3 @@ export ESMF_COMM=mpiuni
 make -j ${CPU_COUNT}
 make check
 make install
-
-# Ideally these files should be moved.
-ln -s ${PREFIX}/bin/binO/*.default/* ${PREFIX}/bin
-ln -s ${PREFIX}/lib/libO/*.default/*.a ${PREFIX}/lib
-ln -s ${PREFIX}/mod/modO/*.default/* ${PREFIX}/mod
-
-if [[ $(uname) == Darwin ]]; then
-ln -s ${PREFIX}/lib/libO/*.default/*.dylib ${PREFIX}/lib
-else
-ln -s ${PREFIX}/lib/libO/*.default/*.so ${PREFIX}/lib
-fi
