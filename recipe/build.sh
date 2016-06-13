cbd4cec24c1dae29097d1c0bb6ca880dcd91bc71
bekozi <benkoziol@gmail.com>
2016-06-13 10:41:24 -0600

    ENH: Parallel ESMF builds #9
    Adding parallel builds for linux and osx. Not convinced osx is going to work.

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,7 +11,12 @@ export ESMF_NETCDF="split"
 export ESMF_NETCDF_INCLUDE=${PREFIX}/include
 export ESMF_NETCDF_LIBPATH=${PREFIX}/lib
 
-export ESMF_COMM=mpiuni
+if [ "$(uname)" == "Darwin" ]; then
+#    export ESMF_COMM=mpiuni
+    export ESMF_COMM=openmpi
+else
+    export ESMF_COMM=mpich2
+fi
 
 make -j ${CPU_COUNT}
 make check
