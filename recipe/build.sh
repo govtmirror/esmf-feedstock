9d5d20744801cf511126683df7bff8bf0b7fd9ac
bekozi <benkoziol@gmail.com>
2016-07-06 09:10:48 -0600

    wip: switching branches

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,6 +16,19 @@ export ESMF_COMM=mpiuni
 make
 #make check
 make install
-otool -L ${PREFIX}/bin/ESMF_Info
+#otool -L ${PREFIX}/bin/ESMF_Info
 #ldd ${PREFIX}/bin/ESMF_Info
 #exit 1
+#  - conda create -n esmf --yes -c conda-forge esmf
+#  - source activate esmf
+#  - find /Users/travis/miniconda3/envs/esmf -name "*libesmf*"
+_ESMF_LIB_PATH=${PREFIX}/lib/libesmf.dylib
+#_ESMF_ORIGINAL_LIB_PATH=/Users/travis/miniconda3/conda-bld/work/esmf/lib/libO/Darwin.gfortran.64.mpiuni.default/libesmf.dylib
+_CURRENT_DIRECTORY=`pwd`
+_ESMF_ORIGINAL_LIB_PATH=`find ${_CURRENT_DIRECTORY}/lib -name '*libesmf.dylib'`
+_ESMF_INFO_PATH=${PREFIX}/bin/ESMF_Info
+ESMF_Info
+#otool -D ${ESMF_LIB_PATH}
+#otool -L ${ESMF_INFO_PATH}
+install_name_tool -change ${_ESMF_ORIGINAL_LIB_PATH} ${_ESMF_LIB_PATH} ${_ESMF_INFO_PATH}
+#  - ESMF_Info
